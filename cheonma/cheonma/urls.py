11a796c566922a2558c79f4b50069b2791f555d9
haejun1 <a01027833227@gmail.com>
2024-01-16 17:59:55 +0900

    create django app

--- cheonma/cheonma/urls.py
+++ cheonma/cheonma/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('rest_framework.urls')),
+    path('', include('cheonma_app.urls')),
 ]
--- cheonma/cheonma_app/views.py
+++ cheonma/cheonma_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+class HomeAPIView(APIView):
+    def get(self, request):
+        data = {'message':"this is test!"}
+        return Response(data, status=status.HTTP_200_OK)
